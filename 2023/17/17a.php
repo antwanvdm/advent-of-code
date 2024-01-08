1bd4688bfdb84eb91b42d1a47f5636b0c10a574a
antwanvdm <antwanvdm@gmail.com>
2024-01-08 15:24:31 +0100

    FIXED 17a YEAH! Learned about priorityqueue & using a "Set" like way of checking visited. Lastly: needed to check the combination of row/column/direction and last 3 paths to make sure we have all required parameters combined in the Dijkstra logic.

--- 2023/17/17a.php
+++ 2023/17/17a.php
@@ -1,34 +1,36 @@
 <?php
-$lavaPool = preg_split("/\r\n|\n|\r/", file_get_contents('17-data-sample.txt'));
+$lavaPool = preg_split("/\r\n|\n|\r/", file_get_contents('17-data.txt'));
 $lavaLines = array_map(fn($line) => str_split($line), $lavaPool);
 
 /**
  * @param $path
+ * @param $lastMoves
  * @param $visited
  * @return bool
  */
-function in_visited_array($path, &$visited): bool
+function in_visited_array($path, $lastMoves, &$visited): bool
 {
-    foreach ($visited as $item) {
-        $checkPath = [$path['row'], $path['column']];
-        if ($item === $checkPath) {
-            return true;
-        }
+    //Combine row, column, direction & last 3 moves to have a unique check
+    $hash = $path['row'] . '-' . $path['column'] . '-' . $path['direction'] . '-' . json_encode($lastMoves);
+    if (isset($visited[$hash])) {
+        return true;
     }
 
-    $visited[] = [$path['row'], $path['column']];
+    $visited[$hash] = 1;
     return false;
 }
 
+/**
+ * @param $path
+ * @param $paths
+ * @param $visited
+ * @param $lavaLines
+ * @return void
+ */
 function move($path, &$paths, &$visited, $lavaLines): void
 {
     $lastMoves = array_slice($path['lastMoves'], -3, 3);
     $lastThreeMovesAreSame = count($lastMoves) === 3 && count(array_unique($lastMoves)) === 1;
-
-    $up = $lavaLines[$path['row'] - 1][$path['column']] ?? false;
-    $down = $lavaLines[$path['row'] + 1][$path['column']] ?? false;
-    $left = $lavaLines[$path['row']][$path['column'] - 1] ?? false;
-    $right = $lavaLines[$path['row']][$path['column'] + 1] ?? false;
 
     $options = [
         'right' => ['up', 'down', 'right'],
@@ -36,58 +38,56 @@ function move($path, &$paths, &$visited, $lavaLines): void
         'up' => ['right', 'left', 'up'],
         'down' => ['right', 'left', 'down'],
     ];
-    $keys = [
+    $directions = [
         'up' => [-1, 0],
         'down' => [1, 0],
         'left' => [0, -1],
         'right' => [0, 1],
     ];
 
+    //Get all next moves based on the current direction & remove the last option when limit has been reached
     $pathOptions = $options[$path['direction']];
     if ($lastThreeMovesAreSame) {
         array_pop($pathOptions);
     }
 
     foreach ($pathOptions as $pathOption) {
-        $nextPath = [
-            'direction' => $pathOption,
-            'row' => $path['row'] + $keys[$pathOption][0],
-            'column' => $path['column'] + $keys[$pathOption][1],
-            'heatLoss' => $path['heatLoss'] + $$pathOption ?? 0,
-            'lastMoves' => array_merge($path['lastMoves'], [$pathOption])
-        ];
-        if ($$pathOption !== false && !in_visited_array($nextPath, $visited)) {
-            $paths[] = $nextPath;
-            usort($paths, fn($a, $b) => $a['heatLoss'] < $b['heatLoss'] ? -1 : 1);
+        $nextRow = $path['row'] + $directions[$pathOption][0];
+        $nextColumn = $path['column'] + $directions[$pathOption][1];
+
+        //Check if next would be within boundaries
+        if ($nextRow >= 0 && $nextRow < count($lavaLines) && $nextColumn >= 0 && $nextColumn < count($lavaLines[0])) {
+            $nextPath = [
+                'direction' => $pathOption,
+                'row' => $nextRow,
+                'column' => $nextColumn,
+                'heatLoss' => $path['heatLoss'] + $lavaLines[$nextRow][$nextColumn],
+                'lastMoves' => array_merge($path['lastMoves'], [$pathOption])
+            ];
+            //Add path if we didn't visit it before
+            if (!in_visited_array($nextPath, $lastMoves, $visited)) {
+                $paths->insert($nextPath, -$nextPath['heatLoss']); //Sort with negative number else we get the longest path
+            }
         }
     }
 }
 
-$paths = [
-    ['row' => 0, 'column' => 1, 'direction' => 'right', 'heatLoss' => 0, 'lastMoves' => ['right']],
-    ['row' => 1, 'column' => 0, 'direction' => 'down', 'heatLoss' => 0, 'lastMoves' => ['down']],
-];
-$leastHeatLoss = 0;
+//Use priority queue to make sure sorting will go faster (eg: automatically)
+$paths = new \SplPriorityQueue();
+$paths->insert(['row' => 0, 'column' => 0, 'direction' => 'right', 'heatLoss' => 0, 'lastMoves' => []], 0);
+$paths->insert(['row' => 0, 'column' => 0, 'direction' => 'down', 'heatLoss' => 0, 'lastMoves' => []], 0);
+
 $visited = [];
-while (count($paths) > 0) {
-    $path = array_shift($paths);
+while (!$paths->isEmpty()) {
+    //Get the path with lowest heatLoss from the queue
+    $path = $paths->extract();
+
     //Break out once the first arrived (which should be the cheapest)
     if ($path['row'] === count($lavaLines) - 1 && $path['column'] === count($lavaLines[0]) - 1) {
-        $leastHeatLoss = $path['heatLoss'];
-        print_r($path['lastMoves']);
+        echo $path['heatLoss'];
         break;
     }
 
     //Make the moves (new paths)
     move($path, $paths, $visited, $lavaLines);
-    $test = '';
 }
-
-echo $leastHeatLoss;
-
-
-//TODO: kijken naar wat de beste optie is (optellen waardes per keer en bepalen wat de volgende logische is)
-
-
-//1440 TOO HIGH
-//1261 TOO HIGH
